ec82e08bfdb19877de0187e8e4768e28360691d8
saimhasan09 <saimhasannahid@gmail.com>
2021-09-13 03:39:15 +0600

    second-edit

--- js/app.js
+++ js/app.js
@@ -15,12 +15,12 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${product.image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h4>${product.title}</h4>
       <p>Category: ${product.category}</p>
       <p>Rating: ${product.rating.rate}</p>
       <p>Rating count: ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success"><i class="fa fa-shopping-cart"></i> add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
 
       `;
